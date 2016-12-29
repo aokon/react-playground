93921d246527902be656e634410552b4eed4cac0
Adam Okoń <adam.okon@elegantcode.pl>
2016-12-29 11:01:34 +0100

    add loading posts

--- poster-fronted/src/components/posts_index.js
+++ poster-fronted/src/components/posts_index.js
@@ -1,5 +1,17 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchPosts } from "../actions/index";
 
-export default () => {
-  return <div>List of the posts</div>;
+class PostIndex extends Component {
+  componentWillMount() { //fires once when component is mounted
+    this.props.fetchPosts();
+  }
+
+  render() {
+    return (
+      <div>List of the posts</div>
+    );
+  }
 }
+
+export default connect(null, { fetchPosts })(PostIndex);
