5d92755715dab98f81d52a9efdaafce5b0504c9e
Adam Okoń <adam.okon@elegantcode.pl>
2016-12-29 09:41:55 +0100

    add skeleton for poster-frontend app

---
+++ poster-fronted/src/components/app.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+       <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+            </div>
+          </div>
+       </div>
+    );
+  }
+}
+
+export default App;
---
+++ poster-fronted/webpack.config.js
@@ -0,0 +1,36 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
+      }
+    }]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env':{
+          'NODE_ENV': JSON.stringify('development'),
+          'WEATHER_API_KEY': JSON.stringify(process.env.WEATHER_API_KEY)
+        }
+    }),
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
+};
