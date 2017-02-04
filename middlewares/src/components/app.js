eb2fa30b971ecfb0ace62bff23c8cdd38398964b
Adam Okoń <adam.okon@elegantcode.pl>
2017-02-04 13:21:09 +0100

    add loading users for middlewares app

---
+++ middlewares/src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import UsersList from './users_list';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="middlewares">
+        <UsersList />
+      </div>
+    );
+  }
+}
