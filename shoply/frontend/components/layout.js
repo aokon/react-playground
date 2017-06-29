07babdf5595212269f438f9a3b7a9f75c30a51c4
Adam Okoń <adam.okon@elegantcode.pl>
2017-06-29 12:52:12 +0200

    Add setup and config styles in next.js way

--- shoply/frontend/components/layout.js
+++ shoply/frontend/components/layout.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from './header'
 import Footer from './footer'
+import stylesheet from 'styles/index.scss'
 
 export default ({ children, title = 'This is default title' }) => (
   <div>
@@ -8,6 +9,7 @@ export default ({ children, title = 'This is default title' }) => (
       <title>{ title }</title>
       <meta charSet='utf-8' />
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
     </Head>
     <Header />
 
