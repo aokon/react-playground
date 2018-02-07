0101837ed08d33398fd5de86976a0f74fa857c92
Adam Okoń <adam.okon@elegantcode.pl>
2018-02-07 17:18:53 +0100

    wish_list: store wish list data in the localstorage

--- wish_list/src/index.js
+++ wish_list/src/index.js
@@ -1,23 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { onSnapshot } from 'mobx-state-tree'
 import registerServiceWorker from './registerServiceWorker';
 
 import App from './components/App';
 import { WishList } from './models/WishList'
 
-  const list = WishList.create({
-    items: [
-      {
-        name: 'Dummy 1',
-        price: 14.7
-      },
-      {
-        name: 'Dummy 2',
-        price: 12.2
-      },
-    ]
-  })
+let initialState = {
+  items: [
+    {
+      name: 'Dummy 1',
+      price: 14.7
+    },
+    {
+      name: 'Dummy 2',
+      price: 12.2
+    },
+  ]
+}
+
+if(localStorage.getItem('wish-list-app')) {
+  const json = JSON.parse(localStorage.getItem('wish-list-app'))
+
+  // check if the loaded data from localstorage have valid schema
+  if(WishList.is(json)) {
+    initialState = json
+  }
+}
+
+const list = WishList.create(initialState)
+
+onSnapshot(list, snapshot => {
+  localStorage.setItem('wish-list-app', JSON.stringify(snapshot))
+})
 
 ReactDOM.render(<App wishList={list} />, document.getElementById('root'));
 registerServiceWorker();
