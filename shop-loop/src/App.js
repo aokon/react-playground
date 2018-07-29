4dd9c2bc03ba93cf3b0c5edb8cd0c7b4b67fcec0
Adam Okoń <adam.okon@elegantcode.pl>
2018-07-29 20:05:28 +0200

    shop-loop: add cart route and basic view

--- shop-loop/src/App.js
+++ shop-loop/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import Home from './pages/Home';
 import About from './pages/About';
+import Cart from './pages/Cart';
 import ProductLanding from './pages/ProductLanding';
 
 export default () => (
@@ -13,11 +14,13 @@ export default () => (
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
+          <li><Link to="/cart">Cart</Link></li>
         </ul>
       </nav>
       <Route exact path="/" component={Home} />
       <Route exact path="/products/:id" component={ProductLanding} />
       <Route path="/about" component={About} />
+      <Route path="/cart" component={Cart} />
     </div>
   </Router>
 );
---
+++ shop-loop/src/pages/Cart/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default () => (
+  <div className="about">
+    <h1>Cart</h1>
+  </div>
+);
