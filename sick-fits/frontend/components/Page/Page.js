d5ef7d53b823ded414d0bde965a1668d8254685c
Adam Okoń <adam.okon@elegantcode.pl>
2018-11-08 20:36:02 +0100

    sick-fits: add meta tags

--- sick-fits/frontend/components/Page/Page.js
+++ sick-fits/frontend/components/Page/Page.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Header from '~/components/Header/Header';
+import Meta from '~/components/Meta/Meta';
 
 const Page = props => (
   <div className="page">
+    <Meta />
     <Header />
     {props.children}
   </div>
