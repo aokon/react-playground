c2a8168925246749b33cf6045217927fd8c814a8
Adam Okoń <adam.okon@elegantcode.pl>
2018-11-11 19:48:48 +0100

    sick-fits: add basic styled-components

--- sick-fits/frontend/components/Page/Page.js
+++ sick-fits/frontend/components/Page/Page.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import Header from '~/components/Header/Header';
 import Meta from '~/components/Meta/Meta';
+import styled from 'styled-components';
+
+const StyledPage = styled.div`
+  background: #fff;
+  color: #000;
+`;
+
+const InnerView = styled.div`
+  max-width: 1000px;
+  padding: 2rem;
+  margin: 0 auto;
+`;
 
 const Page = props => (
-  <div className="page">
+  <StyledPage>
     <Meta />
     <Header />
-    {props.children}
-  </div>
+    <InnerView>
+      {props.children}
+    </InnerView>
+  </StyledPage>
 );
 
 export default Page;
